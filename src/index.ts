a596a39529f49adc4c1c68c4420a1e325b2b122f
Jonath-z <jonathanzihindula95@gmail.com>
2023-12-18 11:52:28 +0200

    refactor: improve process response function and fix type errors

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,79 @@
 import {
-    Canister,
-    ic,
-    Manual,
-    None,
-    Principal,
-    query,
-    Some,
-    update,
-    text,
-    Ok
-} from 'azle';
+  Canister,
+  ic,
+  None,
+  Principal,
+  query,
+  Some,
+  update,
+  text,
+  Result,
+  Err,
+  Ok,
+} from "azle";
 import {
-    HttpResponse,
-    HttpTransformArgs,
-    managementCanister
-} from 'azle/canisters/management';
+  HttpResponse,
+  HttpTransformArgs,
+  managementCanister,
+} from "azle/canisters/management";
 
 export default Canister({
-    translate: update([text, text, text], text, async (api_key, q, target_language) => {
-        const response =  await ic.call(managementCanister.http_request, {
-            args: [
-                {
-                    url: `https://translation.googleapis.com/language/translate/v2?q=${q}&target=${target_language}&key=${api_key}`,
-                    max_response_bytes: Some(2_000n),
-                    method: {
-                        get: null
-                    },
-                    headers: [],
-                    body: None,
-                    transform: Some({
-                        function: [ic.id(), 'translateTransform'] as [
-                            Principal,
-                            string
-                        ],
-                        context: Uint8Array.from([])
-                    })
-                }
-            ],
-            cycles: 50_000_000n
-        });
+  translate: update(
+    [text, text, text],
+    Result(text, text),
+    async (api_key, q, target_language) => {
+      const response = await ic.call(managementCanister.http_request, {
+        args: [
+          {
+            url: `https://translation.googleapis.com/language/translate/v2?q=${q}&target=${target_language}&key=${api_key}`,
+            max_response_bytes: Some(2_000n),
+            method: {
+              get: null,
+            },
+            headers: [],
+            body: None,
+            transform: Some({
+              function: [ic.id(), "translateTransform"] as [Principal, string],
+              context: Uint8Array.from([]),
+            }),
+          },
+        ],
+        cycles: 50_000_000n,
+      });
 
-        return processResponse(response.body);
-    }),
-    translateTransform: query([HttpTransformArgs], HttpResponse, (args) => {
-        return {
-            ...args.response,
-            headers: []
-        };
-    })
+      return processResponse(response.body);
+    }
+  ),
+  translateTransform: query([HttpTransformArgs], HttpResponse, (args) => {
+    return {
+      ...args.response,
+      headers: [],
+    };
+  }),
 });
 
-interface TranslationResponse {
-    data?: {
-        translations?: Array<{ translatedText: string }>;
-    };
-}
+type TranslationResponse = {
+  data?: {
+    translations?: Array<{ translatedText: string }>;
+  };
+};
 
-function processResponse(blob: Blob): string {
-    try {
-        const textDecoder = new TextDecoder('utf-8');
-        const text = textDecoder.decode(blob);
-        const jsonData: TranslationResponse = JSON.parse(text);
+function processResponse(blob: Uint8Array) {
+  try {
+    const text = Buffer.from(blob.buffer).toString("utf-8");
+    const jsonData = JSON.parse(text) as TranslationResponse;
 
-        if (jsonData.data?.translations?.length > 0) {
-            const translatedText = jsonData.data.translations[0].translatedText;
-            return translatedText;
-        } else {
-            return "Error: The expected data is not present in the response.";
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        return "Error: An exception occurred while processing the response.";
+    if (
+      jsonData &&
+      jsonData.data?.translations &&
+      jsonData.data?.translations?.length > 0
+    ) {
+      const translatedText = jsonData.data.translations[0].translatedText;
+      return Ok(translatedText);
+    } else {
+      return Err("The expected data is not present in the response.");
     }
+  } catch (error) {
+    return Err("An exception occurred while processing the response.");
+  }
 }
